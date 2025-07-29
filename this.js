e2b1d473898fab2947eb889b77f7df1e9d75cbb5
Muhammed Sinan T K <sinantkthonikadavath@gmail.com>
2025-07-29 05:48:33 +0530

    apply and bind methhods addded

--- this.js
+++ this.js
@@ -6,7 +6,7 @@ const user={
 }
 
 function Call(name,age){
-    console.log(`Hello,,  My name is ${this.name}. I am ${this.age}old`);
+    console.log(`Hello, I am ${this.name}. I am ${this.age} years old`);
 }
 
 Call.call(user,user.name,user.age);
@@ -14,3 +14,33 @@ Call.call(user,user.name,user.age);
 //-------------------------------------------------------------------------------//
 
 //Apply method//
+
+
+const user1 = {
+    name : "Arjun",
+    age: 18
+}
+
+function Apply(name,age){
+    console.log(`Hello, I am ${name}. I am ${age} years old`);
+}
+
+Apply.apply(user1,[user1.name,user1.age]);
+
+//-----------------------------------------------------------------------------------//
+
+//bind method//
+
+const user2 = { 
+    name: "Ayoob",
+    age: 22
+}
+
+function Bind(name,age){
+    console.log(`Hello, I am ${name}. I am ${age} years old`)
+};
+
+
+const Binded  = Bind.bind(user2,user2.name,user2.age);
+
+Binded();
